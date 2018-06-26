014e279e42c68bbf503e732c156a1e81aa3cf4eb
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-06-26 15:11:22 +0800

    added qualifiers

---
+++ app/src/main/java/com/cogitator/googletransitionapi/di/qualifier/LocationNotificationChannelId.kt
@@ -0,0 +1,10 @@
+package com.cogitator.googletransitionapi.di.qualifier
+
+import javax.inject.Qualifier
+
+/**
+ * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 26/06/2018 (MM/DD/YYYY)
+ */
+@Qualifier
+@Retention
+annotation class LocationNotificationChannelId
