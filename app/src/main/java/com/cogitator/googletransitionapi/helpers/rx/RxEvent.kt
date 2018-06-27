5f460cc013e3ecc8589b15b230073a28aba5b208
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-06-27 10:14:25 +0800

    added rxevent

---
+++ app/src/main/java/com/cogitator/googletransitionapi/helpers/rx/RxEvent.kt
@@ -0,0 +1,6 @@
+package com.cogitator.googletransitionapi.helpers.rx
+
+/**
+ * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 27/06/2018 (MM/DD/YYYY)
+ */
+data class RxEvent(val type: EventType, val message: String = "")
