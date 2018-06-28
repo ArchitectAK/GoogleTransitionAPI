78cd7f803deb174b9f204c12d0e819098539d1ec
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-06-28 14:14:17 +0800

    added transition services

---
+++ app/src/main/java/com/cogitator/googletransitionapi/model/data/transitions/TransitionIntentServiceModule.kt
@@ -0,0 +1,11 @@
+package com.cogitator.googletransitionapi.model.data.transitions
+
+import dagger.Module
+
+/**
+ * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 28/06/2018 (MM/DD/YYYY)
+ */
+@Module
+class TransitionIntentServiceModule {
+
+}
